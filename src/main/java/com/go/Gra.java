2b2fdfa9c62aa777f6a86f8a4ac7431b84398e96
Reicsen <272333@student.pwr.edu.pl>
2023-12-18 22:10:56 +0100

    Zakodowanie działania gry

--- src/main/java/com/go/Gra.java
+++ src/main/java/com/go/Gra.java
@@ -1,10 +1,12 @@
 package com.go;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class Gra implements Runnable
 {
-    private boolean koniecGry;
     private String aktywnyKolor;
     private IPlansza plansza;
     private Socket gracz1;
@@ -14,7 +16,6 @@ public class Gra implements Runnable
     {
         this.aktywnyKolor="czarny";
         this.plansza=new Plansza();
-        this.koniecGry=false;
         this.gracz1=s1;
         this.gracz2=s2;
     }
@@ -38,10 +39,97 @@ public class Gra implements Runnable
 
     private void dodajPionek(int nrpola)
     {
-        if (sprawdzPoprawnosc(nrpola))
+        plansza.dodajPionek(this.aktywnyKolor, nrpola%19, nrpola/19);
+        this.zmienKolor();
+    }
+
+    @Override
+    public void run()
+    {
+        try
         {
-            plansza.dodajPionek(this.aktywnyKolor, nrpola%19, nrpola/19);
-            this.zmienKolor();
+            DataInputStream odbieranieOdGracza1 = new DataInputStream(gracz1.getInputStream());
+            DataOutputStream wysylanieDoGracza1 = new DataOutputStream(gracz1.getOutputStream());
+            DataInputStream odbieranieOdGracza2 = new DataInputStream(gracz2.getInputStream());
+            DataOutputStream wysylanieDoGracza2 = new DataOutputStream(gracz2.getOutputStream());
+
+            wysylanieDoGracza1.writeInt(2);
+            int sygnal;
+            int ruch;
+
+            while (true)
+            {
+                if ("czarny".equals(aktywnyKolor))
+                {
+                    sygnal=odbieranieOdGracza1.readInt();
+                    if (sygnal==-1)
+                    {
+                        wysylanieDoGracza1.writeInt(0);
+                        wysylanieDoGracza1.writeInt(-1);
+                        wysylanieDoGracza2.writeInt(0);
+                        wysylanieDoGracza2.writeInt(1);
+                        break;
+                    }
+                    if (sygnal==0)
+                    {
+                        zmienKolor();
+                        wysylanieDoGracza2.writeInt(2);
+                    }
+                    if (sygnal==1)
+                    {
+                        ruch=odbieranieOdGracza1.readInt();
+                        if (sprawdzPoprawnosc(ruch))
+                        {
+                            wysylanieDoGracza1.writeInt(0);
+                            wysylanieDoGracza1.writeInt(ruch);
+                            wysylanieDoGracza2.writeInt(1);
+                            wysylanieDoGracza2.writeInt(ruch);
+                            dodajPionek(ruch);
+                        }
+                        else
+                        {
+                            wysylanieDoGracza1.writeInt(-1);
+                        }
+                    }
+                }
+                else
+                {
+                    sygnal=odbieranieOdGracza2.readInt();
+                    if (sygnal==-1)
+                    {
+                        wysylanieDoGracza2.writeInt(0);
+                        wysylanieDoGracza2.writeInt(-1);
+                        wysylanieDoGracza1.writeInt(0);
+                        wysylanieDoGracza1.writeInt(1);
+                        break;
+                    }
+                    if (sygnal==0)
+                    {
+                        zmienKolor();
+                        wysylanieDoGracza1.writeInt(2);
+                    }
+                    if (sygnal==1)
+                    {
+                        ruch=odbieranieOdGracza2.readInt();
+                        if (sprawdzPoprawnosc(ruch))
+                        {
+                            wysylanieDoGracza2.writeInt(0);
+                            wysylanieDoGracza2.writeInt(ruch);
+                            wysylanieDoGracza1.writeInt(1);
+                            wysylanieDoGracza1.writeInt(ruch);
+                            dodajPionek(ruch);
+                        }
+                        else
+                        {
+                            wysylanieDoGracza2.writeInt(-1);
+                        }
+                    }
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
