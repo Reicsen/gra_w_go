cf01533ea118c25524dba68cd8bec9cee2c8ed7f
Anna Swierzy <swiezanka@gmail.com>
2024-01-15 22:08:48 +0100

    Poprawa błędów z zasadzie ko

--- src/main/java/com/go/Poprawnosc/Ko.java
+++ src/main/java/com/go/Poprawnosc/Ko.java
@@ -15,38 +15,45 @@ public class Ko implements IPoprawnosc{
         planszaDwaRuchyWczesniej = null;
     }
 
-    //TODO poprawić błędy i zrobić z usuwaniem
+    //TODO zrobić z usuwaniem
     //najlepiej sprawdzic czy jakiś obok ma 1 oddech
     // wtedy nie sprawdzać go w forze
     //ale sprawdzic czy to przeciwnik
+
     public boolean sprawdzPoprawnosc(IPlansza plansza, int x, int y, String kolor){
 
         boolean s = true;
 
+        //jeżeli było mniej niż dwa ruchy to niemożliwe jest ko
         if( !(planszaDwaRuchyWczesniej == null)){
             s = false;
 
             List<IPole> wczesniej = planszaDwaRuchyWczesniej.podajPola();
             List<IPole> teraz = plansza.podajPola();
 
-            //sprawdzamy czy wszystkie pionki są tak samo kiedy dodamy pionek na x y
-
+            //sprawdzamy czy pionek który dodajemy znajdował się na planszy wczesniej
             int miejsceDodanegoPionka = 19*y + x;
 
+            //jeżeli miejsce było puste lub był tam pionek przeciwnika to niemożliwe jest ko
             if((wczesniej.get(miejsceDodanegoPionka).podajPionek() == null ) || !(wczesniej.get(miejsceDodanegoPionka).podajPionek().equals(kolor))){
                 s = true;
             }
 
             for(int i = 0; i < 361; i++){
+                //miejsce gdzie dodajemy pionek się różni bo jeszcze go nie dodaliśmy więc to miesce pomijamy
                 if(i == miejsceDodanegoPionka){
                     i++;
                 }
 
-                if( !(wczesniej.get(i).equals(teraz.get(i))) ){
+                //sprawdzamy czy wszystkie miejsca są takie same
+                //jeżeli nie są to niemożliwe jest ko
+                if(!(wczesniej.get(i).podajPionek() == null && teraz.get(i).podajPionek() == null) || 
+                !(wczesniej.get(i).podajPionek() != null && wczesniej.get(i).podajPionek() != null &&
+                (wczesniej.get(i).podajPionek().equals(teraz.get(i).podajPionek()))) )
+                {
                     s = true;
                 }
             }
-
         }
 
         planszaDwaRuchyWczesniej = planszaRuchWcześniej;
