a6a5b06dfcea180278c7ffeaf9603d0f9cec80f4
Anna Swierzy <swiezanka@gmail.com>
2024-01-18 18:28:50 +0100

    Zmiana liczenia terytorium

--- src/main/java/com/go/ITerytorium.java
+++ src/main/java/com/go/ITerytorium.java
@@ -1,10 +1,20 @@
 package com.go;
 
+import java.util.ArrayList;
+
 /**
  * ITerytorium
  */
 public interface ITerytorium {
 
+    /*
+     * Na podstawie planszy oblicza się terytorium dla gracza o pionach w podanym kolorze
+     */
     int obliczTerytorium(IPlansza plansza, String kolor);
+
+    /*
+     * Na podstawie planszy określa się które piony znajdują się na terytorium gracza o pionach w podanym kolorze
+     */
+    ArrayList <Integer> PionyNaTerytoriumPrzeciwnika(IPlansza plansza, String kolor);
     
 }
--- src/main/java/com/go/Terytorium.java
+++ src/main/java/com/go/Terytorium.java
@@ -1,7 +1,21 @@
 package com.go;
 
+import java.util.ArrayList;
+
 public class Terytorium implements ITerytorium {
+
     public int obliczTerytorium(IPlansza plansza, String kolor){
+        ArrayList<Integer> punktyTerytorium = new ArrayList<>();
+        int terytorium = 0;
         return 0;
+    }
+
+    public ArrayList <Integer> PionyNaTerytoriumPrzeciwnika(IPlansza plansza, String kolor){
+
+        ArrayList <Integer> martwePiony = new ArrayList<>();
+
+
+
+        return martwePiony;
     }
 }
