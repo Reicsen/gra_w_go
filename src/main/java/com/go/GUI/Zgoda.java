d2e5a44605fb476d4548adc93b2701ba3c5ec1d2
Anna Swierzy <swiezanka@gmail.com>
2024-01-27 00:04:33 +0100

    Zmiany w gui

--- src/main/java/com/go/GUI/Zgoda.java
+++ src/main/java/com/go/GUI/Zgoda.java
@@ -7,6 +7,7 @@ import com.go.Gracz;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -16,11 +17,20 @@ public class Zgoda extends GridPane{
     Zgoda(List<String> lista, Gracz gracz, Stage stage, GuiPlansza plansza){
 
         super();
-        System.out.println("Weszlo");
         setAlignment(Pos.CENTER);
         setHgap(0);
         setVgap(0);
         setStyle("-fx-background-color: black;");
+
+        //Tworzymy label na którym będą wyświetlać się komunikaty
+        Label lbl = new Label();
+        lbl.setPrefWidth(920);
+        lbl.setPrefHeight(50);
+        lbl.setStyle("-fx-background-color: white;");
+        lbl.setFont(Font.font(20));
+        lbl.setText("Na fioletowo zaznaczony jest twój teren, a na niebiesko przeciwnika\nKliknij edytuj jesli chcesz samodzielnie ustalić terytorium");
+        setColumnSpan(lbl, 19); // Rozciąganie przez 16 kolumn
+        add(lbl, 0, 0);
 
         //tworzymy plansze
         FactoryButton factory = new StworzButton();
@@ -34,11 +44,10 @@ public class Zgoda extends GridPane{
                 setHalignment(button, HPos.CENTER);
             }
         }
-        System.out.println("plansza");
 
         //tworzymy przycisk Zgoda
         Button b1 = new Button("Zgoda");
-        b1.setPrefWidth(450);
+        b1.setPrefWidth(440);
         b1.setPrefHeight(50);
         b1.setStyle("-fx-background-color: aquamarine");
         b1.setFont(Font.font(20));
@@ -51,10 +60,9 @@ public class Zgoda extends GridPane{
             stage.close();
         });
 
-        System.out.println("b1");
         //tworzymy przecisk wróć do gry
         Button b2 = new Button("Wróć do gry");
-        b2.setPrefWidth(450);
+        b2.setPrefWidth(440);
         b2.setPrefHeight(50);
         b2.setStyle("-fx-background-color: aquamarine");
         b2.setFont(Font.font(20));
@@ -67,10 +75,9 @@ public class Zgoda extends GridPane{
             stage.close();
         });
 
-        System.out.println("b2");
         //tworzymy przecisk edytuj 
         Button b3 = new Button("Edytuj");
-        b3.setPrefWidth(450);
+        b3.setPrefWidth(440);
         b3.setPrefHeight(50);
         b3.setStyle("-fx-background-color: aquamarine");
         b3.setFont(Font.font(20));
@@ -81,6 +88,5 @@ public class Zgoda extends GridPane{
             plansza.zaznaczTeren(gracz.podajListe());
             stage.close();
         });
-        System.out.println("b3");
     }
 }
