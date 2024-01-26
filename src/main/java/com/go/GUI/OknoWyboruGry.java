0fa666017fed79f9f121af1a2df83af47386b7e6
Reicsen <reicsen@gmail.com>
2024-01-26 21:23:05 +0100

    Poprawienie wyboru gry

--- src/main/java/com/go/GUI/OknoWyboruGry.java
+++ src/main/java/com/go/GUI/OknoWyboruGry.java
@@ -1,8 +1,8 @@
 package com.go.GUI;
 
-import java.sql.*;
-import com.go.OkienkoBledu;
 
+import java.util.List;
+import com.go.OkienkoBledu;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -18,112 +18,99 @@ import javafx.stage.Stage;
 
 public class OknoWyboruGry implements IBazyDanychAdapter
 {
+    private List<Integer> idGier;
+    private List<String> zwyciezcyGier;
     private int iloscGier;
-    private ResultSet listaGier;
     
-    public OknoWyboruGry(int ilosc, ResultSet wyniki)
+    public OknoWyboruGry(List<Integer> id, List<String> zwyciezcy)
     {
-        this.iloscGier=ilosc;
-        this.listaGier=wyniki;
+        this.idGier=id;
+        this.zwyciezcyGier=zwyciezcy;
+        this.iloscGier=idGier.size();
     }
 
     public void obsluz()
     {
-        if(iloscGier==0)
+        GridPane siatka = new GridPane();
+        Stage baza = new Stage();
+
+        Label kol1 = new Label("Id gry");
+        kol1.setFont(Font.font("Callibri",FontWeight.BOLD,20));
+        kol1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
+        kol1.setWrapText(true);
+        kol1.setTextAlignment(TextAlignment.CENTER);
+        kol1.setMinWidth(300);
+        kol1.setMaxWidth(300);
+        kol1.setMinHeight(30);
+        kol1.setMaxHeight(30);
+
+        Label kol2 = new Label("Zwycięzca");
+        kol2.setFont(Font.font("Callibri",FontWeight.BOLD,20));
+        kol2.setBackground(new Background(new BackgroundFill(Color.MISTYROSE,null,null)));
+        kol2.setWrapText(true);
+        kol2.setTextAlignment(TextAlignment.CENTER);
+        kol2.setMinWidth(300);
+        kol2.setMaxWidth(300);
+        kol2.setMinHeight(30);
+        kol2.setMaxHeight(30);
+
+        siatka.addRow(0, kol1,kol2);
+
+        EventHandler<ActionEvent> kliknieto = new EventHandler<ActionEvent>()
         {
-            OkienkoBledu.wyswietlBlad("ERROR#404: brak gier w bazie");
-        }
-        else
+            @Override
+            public void handle (ActionEvent event)
+            {
+                IDButton zrodlo = (IDButton) event.getSource();
+                int id = zrodlo.zwrocId();
+                GridPane gridPane1 = new GuiPlanszaOdtworzenia(id);
+                GridPane gridPane2 = new GuiPlanszaOdtworzenia(id);
+                baza.close();
+            }
+        };
+
+        for(int i=0; i<iloscGier; i++)
         {
-            GridPane siatka = new GridPane();
-            Stage baza = new Stage();
+            int id = idGier.get(i);
+            String wygrany = zwyciezcyGier.get(i);
+            
+            IDButton temp1 = new IDButton(id,Integer.toString(id));
+            temp1.setFont(Font.font("Callibri",FontWeight.BOLD,20));
+            temp1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
+            temp1.setWrapText(true);
+            temp1.setTextAlignment(TextAlignment.CENTER);
+            temp1.setMinWidth(300);
+            temp1.setMaxWidth(300);
+            temp1.setMinHeight(30);
+            temp1.setMaxHeight(30);
 
-            Label kol1 = new Label("Id gry");
-            kol1.setFont(Font.font("Callibri",FontWeight.BOLD,20));
-            kol1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
-            kol1.setWrapText(true);
-            kol1.setTextAlignment(TextAlignment.CENTER);
-            kol1.setMinWidth(300);
-            kol1.setMaxWidth(300);
-            kol1.setMinHeight(30);
-            kol1.setMaxHeight(30);
+            IDButton temp2 = new IDButton(id,wygrany);
+            temp2.setFont(Font.font("Callibri",FontWeight.BOLD,20));
+            temp2.setBackground(new Background(new BackgroundFill(Color.MISTYROSE,null,null)));
+            temp2.setWrapText(true);
+            temp2.setTextAlignment(TextAlignment.CENTER);
+            temp2.setMinWidth(300);
+            temp2.setMaxWidth(300);
+            temp2.setMinHeight(30);
+            temp2.setMaxHeight(30);
 
-            Label kol2 = new Label("Zwycięzca");
-            kol2.setFont(Font.font("Callibri",FontWeight.BOLD,20));
-            kol2.setBackground(new Background(new BackgroundFill(Color.MISTYROSE,null,null)));
-            kol2.setWrapText(true);
-            kol2.setTextAlignment(TextAlignment.CENTER);
-            kol2.setMinWidth(300);
-            kol2.setMaxWidth(300);
-            kol2.setMinHeight(30);
-            kol2.setMaxHeight(30);
+            temp1.setOnAction(kliknieto);
+            temp2.setOnAction(kliknieto);
 
-            siatka.addRow(0, kol1,kol2);
-
-            EventHandler<ActionEvent> kliknieto = new EventHandler<ActionEvent>()
-            {
-                @Override
-                public void handle (ActionEvent event)
-                {
-                    IDButton zrodlo = (IDButton) event.getSource();
-                    int id = zrodlo.zwrocId();
-                    GridPane gridPane1 = new GuiPlanszaOdtworzenia(id);
-                    GridPane gridPane2 = new GuiPlanszaOdtworzenia(id);
-                    baza.close();
-                }
-            };
-
-            try
-            {
-                while(listaGier.next())
-                {
-                    int id = listaGier.getInt("id");
-                    String wygrany = listaGier.getString("wygrany");
-                    
-                    IDButton temp1 = new IDButton(id,Integer.toString(id));
-                    temp1.setFont(Font.font("Callibri",FontWeight.BOLD,20));
-                    temp1.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE,null,null)));
-                    temp1.setWrapText(true);
-                    temp1.setTextAlignment(TextAlignment.CENTER);
-                    temp1.setMinWidth(300);
-                    temp1.setMaxWidth(300);
-                    temp1.setMinHeight(30);
-                    temp1.setMaxHeight(30);
-
-                    IDButton temp2 = new IDButton(id,wygrany);
-                    temp2.setFont(Font.font("Callibri",FontWeight.BOLD,20));
-                    temp2.setBackground(new Background(new BackgroundFill(Color.MISTYROSE,null,null)));
-                    temp2.setWrapText(true);
-                    temp2.setTextAlignment(TextAlignment.CENTER);
-                    temp2.setMinWidth(300);
-                    temp2.setMaxWidth(300);
-                    temp2.setMinHeight(30);
-                    temp2.setMaxHeight(30);
-
-                    temp1.setOnAction(kliknieto);
-                    temp2.setOnAction(kliknieto);
-
-                    siatka.addRow(listaGier.getRow(), temp1,temp2);
-                }
-            }
-            catch(SQLException e)
-            {
-                e.printStackTrace();
-                OkienkoBledu.wyswietlBlad("Wystąpił nieprzewidziany błąd");
-            }
-
-            Scene scena = new Scene(siatka);
-            baza.setTitle("");
-            siatka.setMinWidth(600);
-            siatka.setMaxWidth(600);
-            siatka.setMinHeight((iloscGier+1)*30+40);
-            siatka.setMaxHeight((iloscGier+1)*30+40);
-            baza.setScene(scena);
-            baza.setMinWidth(600);
-            baza.setMaxWidth(600);
-            baza.setMinHeight((iloscGier+1)*30+40);
-            baza.setMaxHeight((iloscGier+1)*30+40);
-            baza.show();
+            siatka.addRow(i+1, temp1,temp2);
         }
+
+        Scene scena = new Scene(siatka);
+        baza.setTitle("");
+        siatka.setMinWidth(600);
+        siatka.setMaxWidth(600);
+        siatka.setMinHeight((iloscGier+1)*30+40);
+        siatka.setMaxHeight((iloscGier+1)*30+40);
+        baza.setScene(scena);
+        baza.setMinWidth(600);
+        baza.setMaxWidth(600);
+        baza.setMinHeight((iloscGier+1)*30+40);
+        baza.setMaxHeight((iloscGier+1)*30+40);
+        baza.show();
     }
 }
--- src/main/java/com/go/GUI/PobieranieDanychZBazy.java
+++ src/main/java/com/go/GUI/PobieranieDanychZBazy.java
@@ -1,6 +1,9 @@
 package com.go.GUI;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.go.OkienkoBledu;
 
 public class PobieranieDanychZBazy implements IBazyDanychAdapter
@@ -15,11 +18,31 @@ public class PobieranieDanychZBazy implements IBazyDanychAdapter
             ResultSet ile = kwerenda.executeQuery("EXECUTE ilosc_gier");
             ile.first();
             int ilosc = ile.getInt(1);
-            
-            IBazyDanychAdapter wyborGry = new OknoWyboruGry(ilosc, wyniki);
-            wyborGry.obsluz();
+
+            if(ilosc==0)
+            {
+                OkienkoBledu.wyswietlBlad("ERROR#404: brak gier w bazie");
+            }
+
+            else
+            {
+                List<Integer> id = new ArrayList<Integer>();
+                List<String> zwyciezcy = new ArrayList<String>();
+
+                while(wyniki.next())
+                {
+                    int idGry = wyniki.getInt("id");
+                    String wygrany = wyniki.getString("wygrany");
+
+                    id.add(idGry);
+                    zwyciezcy.add(wygrany);
+                }
+                
+                IBazyDanychAdapter wyborGry = new OknoWyboruGry(id, zwyciezcy);
+                wyborGry.obsluz();
+            }
             wyniki.close();
-            ile.close();
+                ile.close();
         }
         catch(SQLException e)
         {
