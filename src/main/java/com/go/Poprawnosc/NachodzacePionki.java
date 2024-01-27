390923dfab320cc95d4198f9be06462e2d0e4e78
Anna Swierzy <swiezanka@gmail.com>
2024-01-27 23:58:28 +0100

    Poprawa ko

--- src/main/java/com/go/Poprawnosc/NachodzacePionki.java
+++ src/main/java/com/go/Poprawnosc/NachodzacePionki.java
@@ -3,13 +3,14 @@ package com.go.Poprawnosc;
 import com.go.IPlansza;
 
 public class NachodzacePionki implements IPoprawnosc{
+    IPoprawnosc ko = new Ko();
     public boolean sprawdzPoprawnosc(IPlansza plansza, int x, int y, String kolor){
 
         if (plansza.podajPola().get(19*y+x).podajPionek()!=null)
         {
             return false;
         }
-        IPoprawnosc p = new PoprawnaLiczbaOddechow();
+        IPoprawnosc p = new PoprawnaLiczbaOddechow(ko);
         return p.sprawdzPoprawnosc(plansza, x, y, kolor);
     }
 }
