e906f37582c545575453f16300912e55331dc36d
Reicsen <reicsen@gmail.com>
2024-01-30 12:03:09 +0100

    Edycja testów

--- src/test/java/com/go/Gra/TestIlosciPionowWKolorze.java
+++ src/test/java/com/go/Gra/TestIlosciPionowWKolorze.java
@@ -15,6 +15,23 @@ public class TestIlosciPionowWKolorze
         
         for (int x=0; x<19; x++)
         {
+            for (int y=0; y<19; y++)
+            {
+                plansza.dodajPionek("czarny", x, y);
+            }
+        }
+
+        assertEquals(361,obliczenia.iloscPionkowKoloru(plansza,"czarny"));
+    }
+    
+    @Test
+    public void testIlosciPionowWKolorze2()
+    {
+
+        IPlansza plansza = new Plansza();
+        
+        for (int x=0; x<19; x++)
+        {
             for (int y=0; y<3; y++)
             {
                 plansza.dodajPionek("czarny", x, y);
@@ -25,8 +42,28 @@ public class TestIlosciPionowWKolorze
             }
         }
 
-        assertEquals(0,obliczenia.iloscPionkowKoloru(plansza,"czerwony"));
         assertEquals(57,obliczenia.iloscPionkowKoloru(plansza,"czarny"));
         assertEquals(29,obliczenia.iloscPionkowKoloru(plansza,"biały"));
+    }
+    
+    @Test
+    public void testIlosciPionowWKolorze3()
+    {
+
+        IPlansza plansza = new Plansza();
+        
+        for (int x=0; x<19; x++)
+        {
+            for (int y=0; y<7; y++)
+            {
+                plansza.dodajPionek("czarny", x, y);
+                if ((x+y)%2==0)
+                {
+                    plansza.dodajPionek("biały", x, 18-y);
+                }
+            }
+        }
+
+        assertEquals(0,obliczenia.iloscPionkowKoloru(plansza,"czerwony"));
     }
 }
